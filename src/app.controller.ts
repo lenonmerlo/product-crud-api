22538c30a811f771c42dd9acb078ad7c6f8013c0
lenonmerlo <lenontm@gmail.com>
2026-05-22 20:44:36 -0300

    fix: support root and health endpoints

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiExcludeEndpoint, ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('Health')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get()
+  @ApiExcludeEndpoint()
+  rootHealth() {
+    return this.health();
+  }
 
   @Get('health')
   @ApiOperation({ summary: 'Health check' })
