65de104594a26d882a1fe463dab200e128c13470
lenonmerlo <lenontm@gmail.com>
2026-05-22 15:16:23 -0300

    feat: add health check endpoint and clean up app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,14 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { ProducsModule } from './products/products.module';
+import { UploadModule } from './upload/upload.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     PrismaModule,
     ProducsModule,
+    UploadModule,
   ],
   controllers: [AppController],
   providers: [AppService],
