df27a739a424b4bfb79d153fc901e3ba06f2bee6
lenonmerlo <lenontm@gmail.com>
2026-05-22 18:51:36 -0300

    fix: correct products module naming

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
-import { ProducsModule } from './products/products.module';
+import { ProductsModule } from './products/products.module';
 import { UploadModule } from './upload/upload.module';
 import { AuthModule } from './auth/auth.module';
 
@@ -11,7 +11,7 @@ import { AuthModule } from './auth/auth.module';
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     PrismaModule,
-    ProducsModule,
+    ProductsModule,
     UploadModule,
     AuthModule,
   ],
--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -6,4 +6,4 @@ import { ProductsService } from './products.service';
   controllers: [ProductsController],
   providers: [ProductsService],
 })
-export class ProducsModule {}
+export class ProductsModule {}
